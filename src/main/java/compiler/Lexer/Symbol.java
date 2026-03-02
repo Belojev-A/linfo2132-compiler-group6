c553a4d0c88117c35cb35dfb05f3d1ee343ba59d
wilfred33 <wilfrednanwouo@gmail.com>
2026-03-02 12:51:33 +0100

    Update Symbol class for lexer

--- src/main/java/compiler/Lexer/Symbol.java
+++ src/main/java/compiler/Lexer/Symbol.java
@@ -1,4 +1,29 @@
 package compiler.Lexer;
 
 public class Symbol {
+
+    public final TokenType type;
+    public final String value;
+    public final int line;
+    public final int column;
+
+    public Symbol(TokenType type, String value, int line, int column) {
+        this.type = type;
+        this.value = value;
+        this.line = line;
+        this.column = column;
+    }
+
+    // tokens sans valeur pertinente
+    public Symbol(TokenType type, int line, int column) {
+        this(type, null, line, column);
+    }
+
+    @Override
+    public String toString() {
+        if (value != null) {
+            return "<" + type + ", " + value + ">";
+        }
+        return "<" + type + ">";
+    }
 }
